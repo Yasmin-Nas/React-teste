d6c971f8e55f470d3956c8b02164e9f3e6d9608b
Yasmin Santos <yasminnas989@gmail.com>
2023-03-13 11:19:43 -0300

    Study react

---
+++ src/components/Counter.jsx
@@ -0,0 +1,12 @@
+export function Counter() {
+    function increment(){
+        console.log('Incrementing');
+    }
+
+    return(
+        <div>
+            <h2>0</h2>
+            <button type="button">Increment</button>
+        </div>
+    );
+}
